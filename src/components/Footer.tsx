abf8dc98d4782911ac857d6e6d31c68ad1381f45
Ignacio Bockl <ignaciobockl96@gmail.com>
2023-07-24 12:50:55 -0300

    footer and navbar in progress

---
+++ src/components/Footer.tsx
@@ -0,0 +1,8 @@
+
+const Footer = () => {
+  return (
+    <div>Footerr</div>
+  )
+}
+
+export default Footer
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link"
+
+const Navbar = () => {
+  return (
+    <nav className="flexBetween navbar">
+        <div className="flex-1 flexStart gap-10">
+            <Link>
+            
+            </Link>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
